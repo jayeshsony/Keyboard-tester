7866cb791b7fc35601d74f9b2c196f112a0c2f30
Jayesh <164141803+jayeshsony@users.noreply.github.com>
2024-03-26 13:06:33 +0530

    Add files via upload

---
+++ keyboard/script.js
@@ -0,0 +1,477 @@
+
+
+function playSound(audioName)
+{
+  let audio= new Audio(audioName) ;
+  audio.play();
+}
+
+function insertdata(event)
+{
+    switch(event.key)
+    {  
+        
+        case " ":
+        playSound("mmc.mp3");
+        document.getElementById("keyspace").style.backgroundColor="blue";
+       
+        break;
+
+        case "`":
+        playSound("mmc.mp3");
+        document.getElementById("keytidle").style.backgroundColor="blue";
+        break;
+
+        case "1":
+        playSound("mmc.mp3");
+        document.getElementById("key1").style.backgroundColor="blue";
+        break;
+
+        case "2":
+        playSound("mmc.mp3");
+        document.getElementById("key2").style.backgroundColor="blue";
+        break;
+
+        case "3":
+        playSound("mmc.mp3");
+        document.getElementById("key3").style.backgroundColor="blue";
+        break;
+
+        case "4":
+        playSound("mmc.mp3");
+        document.getElementById("key4").style.backgroundColor="blue";
+        break;
+
+        case "5":
+        playSound("mmc.mp3");
+        document.getElementById("key5").style.backgroundColor="blue";
+        break;
+
+        case "6":
+        playSound("mmc.mp3");
+        document.getElementById("key6").style.backgroundColor="blue";
+        break;
+
+        case "7":
+        playSound("mmc.mp3");
+        document.getElementById("key7").style.backgroundColor="blue";
+        break;
+
+
+        case "8":
+        playSound("mmc.mp3");
+        document.getElementById("key8").style.backgroundColor="blue";
+        break;
+
+
+        case "9":
+        playSound("mmc.mp3");
+        document.getElementById("key9").style.backgroundColor="blue";
+        break;
+
+        case "0":
+        playSound("mmc.mp3");
+        document.getElementById("key0").style.backgroundColor="blue";
+        break;
+
+        case "-":
+        playSound("mmc.mp3");
+        document.getElementById("keymin").style.backgroundColor="blue";
+        break;
+
+        case "=":
+        playSound("mmc.mp3");
+        document.getElementById("keyplus").style.backgroundColor="blue";
+        break;
+
+        case "Delete":
+        playSound("mmc.mp3");
+        document.getElementById("keydel").style.backgroundColor="blue";
+        break;
+
+        case "Q":
+        playSound("mmc.mp3");
+        document.getElementById("keyq").style.backgroundColor="blue";
+        break;
+
+        case "q":
+        playSound("mmc.mp3");
+        document.getElementById("keyq").style.backgroundColor="blue";
+        break;
+
+        case "W":
+        playSound("mmc.mp3");   
+        document.getElementById("keyw").style.backgroundColor="blue";
+        break;
+
+        case "w":
+        playSound("mmc.mp3");
+        document.getElementById("keyw").style.backgroundColor="blue";
+        break;
+
+        case "E":
+        playSound("mmc.mp3");
+        document.getElementById("keye").style.backgroundColor="blue";
+        break;
+
+        case "e":
+        playSound("mmc.mp3");
+        document.getElementById("keye").style.backgroundColor="blue";
+        break;
+
+        case "R":
+        playSound("mmc.mp3");
+        document.getElementById("keyr").style.backgroundColor="blue";
+        break;
+
+        case "r":
+        playSound("mmc.mp3");
+        document.getElementById("keyr").style.backgroundColor="blue";
+        break;
+
+
+        case "t":
+        playSound("mmc.mp3");
+        document.getElementById("keyt").style.backgroundColor="blue";
+        break;
+
+        case "T":
+        playSound("mmc.mp3");
+        document.getElementById("keyt").style.backgroundColor="blue";
+        break;
+
+        case "y":
+        playSound("mmc.mp3");
+        document.getElementById("keyy").style.backgroundColor="blue";
+        break;
+
+        case "Y":
+        playSound("mmc.mp3");
+        document.getElementById("keyy").style.backgroundColor="blue";
+        break;
+
+        case "u":
+        playSound("mmc.mp3");
+        document.getElementById("keyu").style.backgroundColor="blue";
+        break;
+
+        case "U":
+        playSound("mmc.mp3");
+        document.getElementById("keyu").style.backgroundColor="blue";
+        break;
+
+        case "I":
+        playSound("mmc.mp3");
+        document.getElementById("keyi").style.backgroundColor="blue";
+        break;
+
+        case "i":
+        playSound("mmc.mp3");
+        document.getElementById("keyi").style.backgroundColor="blue";
+        break;
+
+        case "O":
+        playSound("mmc.mp3");
+        document.getElementById("keyo").style.backgroundColor="blue";
+        break;
+
+        case "o":
+        playSound("mmc.mp3");
+        document.getElementById("keyo").style.backgroundColor="blue";
+        break;
+
+        case "P":
+        playSound("mmc.mp3");
+        document.getElementById("keyp").style.backgroundColor="blue";
+        break;
+
+        case "p":
+        playSound("mmc.mp3");
+        document.getElementById("keyp").style.backgroundColor="blue";
+        break;
+
+        case "[":
+        playSound("mmc.mp3");
+        document.getElementById("keyop").style.backgroundColor="blue";
+        break;
+
+        case "]":
+        playSound("mmc.mp3");
+        document.getElementById("keypo").style.backgroundColor="blue";
+        break;
+        
+        case "\\":
+        playSound("mmc.mp3");
+        document.getElementById("keyline").style.backgroundColor="blue";
+        break;
+
+        case "a":
+        playSound("mmc.mp3");
+        document.getElementById("keya").style.backgroundColor="blue";
+        break;
+
+        case "A":
+        playSound("mmc.mp3");
+        document.getElementById("keya").style.backgroundColor="blue";
+        break;
+
+        case "s":
+        playSound("mmc.mp3");
+        document.getElementById("keys").style.backgroundColor="blue";
+        break;
+
+        case "S":
+        playSound("mmc.mp3");
+        document.getElementById("keys").style.backgroundColor="blue";
+        break;
+
+        case "d":
+        playSound("mmc.mp3");
+        document.getElementById("keyd").style.backgroundColor="blue";
+        break;
+
+        case "D":
+        playSound("mmc.mp3");
+        document.getElementById("keyd").style.backgroundColor="blue";
+        break;
+
+
+        case "f":
+        playSound("mmc.mp3");
+        document.getElementById("keyf").style.backgroundColor="blue";
+        break;
+
+        case "F":
+        playSound("mmc.mp3");
+        document.getElementById("keyf").style.backgroundColor="blue";
+        break;
+
+
+        case "G":
+        playSound("mmc.mp3");
+        document.getElementById("keyg").style.backgroundColor="blue";
+        break;
+
+        case "g":
+        playSound("mmc.mp3");
+        document.getElementById("keyg").style.backgroundColor="blue";
+        break;
+
+
+        case "H":
+        playSound("mmc.mp3");
+        document.getElementById("keyh").style.backgroundColor="blue";
+        break;
+
+        case "h":
+        playSound("mmc.mp3");
+        document.getElementById("keyh").style.backgroundColor="blue";
+        break;
+
+
+        case "j":
+        playSound("mmc.mp3");
+        document.getElementById("keyj").style.backgroundColor="blue";
+        break;
+
+        case "J":
+        playSound("mmc.mp3");
+        document.getElementById("keyj").style.backgroundColor="blue";
+        break;
+
+        case "K":
+        playSound("mmc.mp3");
+        document.getElementById("keyk").style.backgroundColor="blue";
+        break;
+
+        case "k":
+        playSound("mmc.mp3");
+        document.getElementById("keyk").style.backgroundColor="blue";
+        break;
+
+        case "L":
+        playSound("mmc.mp3");
+        document.getElementById("keyl").style.backgroundColor="blue";
+        break;
+
+        case "l":
+        playSound("mmc.mp3");
+        document.getElementById("keyl").style.backgroundColor="blue";
+        break;
+
+        case ";":
+        playSound("mmc.mp3");
+        document.getElementById("keycolon").style.backgroundColor="blue";
+        break;
+
+        case "'":
+        playSound("mmc.mp3");
+        document.getElementById("keyqoute").style.backgroundColor="blue";
+        break;
+
+        case "Enter":
+        playSound("mmc.mp3");
+        document.getElementById("keyenter").style.backgroundColor="blue";
+        break;
+
+        
+        case "Tab":
+        playSound("mmc.mp3");
+        document.getElementById("keytab").style.backgroundColor="blue";
+        break;
+
+        
+        case "CapsLock":
+        playSound("mmc.mp3");
+        document.getElementById("keycapslock").style.backgroundColor="blue";
+        break;
+
+        
+        case "Shift":
+            {
+                playSound("mmc.mp3"); 
+             collection =   document.getElementsByClassName("keyshift");
+             
+               for (let i = 0; i < collection.length; i++)
+                {
+                 collection[i].style.backgroundColor = "BLUE";
+                }
+            }
+        break;
+
+        case "Control":
+            {
+                playSound("mmc.mp3");
+             collection = document.getElementsByClassName("keyctrl");
+               for (let i = 0; i < collection.length; i++)
+                {
+                 collection[i].style.backgroundColor = "BLUE";
+                }
+            }
+        break;
+
+        case "Alt":
+            {
+                playSound("mmc.mp3");
+             collection = document.getElementsByClassName("keyalt");
+               for (let i = 0; i < collection.length; i++)
+                {
+                 collection[i].style.backgroundColor = "BLUE";
+                }
+            }
+        break;
+        
+
+        case "Meta":
+      
+            {
+                playSound("mmc.mp3");
+             collection =  document.getElementsByClassName("keycmd");
+               for (let i = 0; i < collection.length; i++)
+                {
+                    
+                    collection[i].style.backgroundColor = "BLUE";
+                }
+            }
+        break;
+        
+        
+        case "Z":
+        playSound("mmc.mp3");
+        document.getElementById("keyz").style.backgroundColor="blue";
+        break;
+
+        case "z":
+        playSound("mmc.mp3");
+        document.getElementById("keyz").style.backgroundColor="blue";
+        break;
+
+        case "x":
+        playSound("mmc.mp3");
+        document.getElementById("keyx").style.backgroundColor="blue";
+        break;
+
+        case "X":
+        playSound("mmc.mp3");
+        document.getElementById("keyx").style.backgroundColor="blue";
+        break;
+
+        case "c":
+        playSound("mmc.mp3");
+        document.getElementById("keyc").style.backgroundColor="blue";
+        break;
+
+        case "C":
+        playSound("mmc.mp3");
+        document.getElementById("keyc").style.backgroundColor="blue";
+        break;
+
+        case "V":
+        playSound("mmc.mp3");
+        document.getElementById("keyv").style.backgroundColor="blue";
+        break;
+
+        case "v":
+        playSound("mmc.mp3");
+        document.getElementById("keyv").style.backgroundColor="blue";
+        break;
+
+        case "b":
+        playSound("mmc.mp3");
+        document.getElementById("keyb").style.backgroundColor="blue";
+        break;
+
+        case "B":
+        playSound("mmc.mp3");
+        document.getElementById("keyb").style.backgroundColor="blue";
+        break;
+
+        case "N":
+        playSound("mmc.mp3");
+        document.getElementById("keyn").style.backgroundColor="blue";
+        break;
+
+        case "n":
+        playSound("mmc.mp3");
+        document.getElementById("keyn").style.backgroundColor="blue";
+        break;
+
+        case "M":
+        playSound("mmc.mp3");
+        document.getElementById("keym").style.backgroundColor="blue";
+        break;
+
+        case "m":
+        playSound("mmc.mp3");
+        document.getElementById("keym").style.backgroundColor="blue";
+        break;
+
+        case ",":
+        playSound("mmc.mp3");
+        document.getElementById("keycomma").style.backgroundColor="blue";
+        break;
+
+        case ".":
+        playSound("mmc.mp3");
+        document.getElementById("keydot").style.backgroundColor="blue";
+        break;
+
+        case "/":
+        playSound("mmc.mp3");
+        document.getElementById("keyfslash").style.backgroundColor="blue";
+        break;
+
+        
+        case "ContextMenu":
+        playSound("mmc.mp3");
+        document.getElementById("keyfn").style.backgroundColor="blue";
+        break;
+
+       
+
+        
+    }
+}
+
+
+
